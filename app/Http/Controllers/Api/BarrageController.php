52555eb79fe35dba346df64d63a6c3d26ed27dfd
Dmise <2894467595@qq.com>
2024-09-09 17:44:45 +0800

    code update

--- app/Http/Controllers/Api/BarrageController.php
+++ app/Http/Controllers/Api/BarrageController.php
@@ -8,18 +8,7 @@ class BarrageController extends Controller
 {
     public function index(): JsonResponse
     {
-        $response_data = [];
-
-        $barrage = __('barrage');
-        foreach ($barrage as $item => $value) {
-            $item++;
-
-            $response_data[] = [
-                'id' => $item,
-                'is_vip' => !($item === 1 or $item === 2),
-                'text' => $value,
-            ];
-        }
+        $response_data = __('barrage');
 
         return $this->success($response_data);
     }
--- lang/en/barrage.php
+++ lang/en/barrage.php
@@ -2,11 +2,153 @@
 
 return [
     [
-        '1',
-        '2',
+        'text' => '☆☆Get rich overnight↖(^ω^)↗',
     ],
     [
-        '3',
-        '4',
+        'text' => 'Full skills(o^^o)',
+    ],
+    [
+        'text' => '♛♛♛Promise you a prosperous life',
+    ],
+    [
+        'text' => 'Get off work early, no worries',
+    ],
+    [
+        'text' => "It doesn't matter, no need, no need",
+    ],
+    [
+        'text' => 'Retire early',
+    ],
+    [
+        'text' => "Don't lose your temper, just make money",
+    ],
+    [
+        'text' => 'Be ambitious, have brilliance',
+    ],
+    [
+        'text' => 'Loving yourself is the best fortune',
+    ],
+    [
+        'text' => 'Refuse internal consumption☀︎☀︎',
+    ],
+    [
+        'text' => 'Win 500 million in the lottery',
+    ],
+    [
+        'text' => 'Amitabha',
+    ],
+    [
+        'text' => 'Wait for a snow☆･･☆',
+    ],
+    [
+        'text' => 'Invincible in the world♛♚♜',
+    ],
+    [
+        'text' => 'Get off work on time',
+    ],
+    [
+        'text' => 'Daily yoga',
+    ],
+    [
+        'text' => 'Happy day O(∩_∩)O~~',
+    ],
+    [
+        'text' => 'Love yourself a little more every day:-)',
+    ],
+    [
+        'text' => 'Smile and you will be ten years younger',
+    ],
+    [
+        'text' => 'Peace and happiness♖♖♖',
+    ],
+    [
+        'text' => 'All the best',
+    ],
+    [
+        'text' => 'Business is booming☼☼☼',
+    ],
+    [
+        'text' => 'Sleep until you wake up naturally every day',
+    ],
+    [
+        'text' => 'Make fortunes',
+    ],
+    [
+        'text' => 'Eat well and drink well, live forever☾☾☾',
+    ],
+    [
+        'text' => 'Just want to retire☆☆',
+    ],
+    [
+        'text' => 'Conspicuous bag( ；´Д｀)',
+    ],
+    [
+        'text' => 'Reject anxiety',
+    ],
+    [
+        'text' => "Today's personality: cool",
+    ],
+    [
+        'text' => 'Good luck O(∩_∩)O~~',
+    ],
+    [
+        'text' => 'God of Wealth is here☆･･☆',
+    ],
+    [
+        'text' => 'Every road is smooth☼☼☼',
+    ],
+    [
+        'text' => 'Money in pocket',
+    ],
+    [
+        'text' => 'See you year after year☃',
+    ],
+    [
+        'text' => "Don't forget your original intention♥︎♥︎♥︎",
+    ],
+    [
+        'text' => 'Full of energy(^з^)-☆',
+    ],
+    [
+        'text' => 'Confidence☼☼☼',
+    ],
+    [
+        'text' => '┻━┻︵╰(‵□′)╯︵┻━┻',
+    ],
+    [
+        'text' => 'No matter where you are, I am the most beautiful(o^^o)',
+    ],
+    [
+        'text' => 'Wish to find a like-minded person☃︎☃︎☃︎',
+    ],
+    [
+        'text' => 'Every day is a new day',
+    ],
+    [
+        'text' => 'Sleep well♣︎♣︎',
+    ],
+    [
+        'text' => 'Zundu fakedu(⌒▽⌒)',
+    ],
+    [
+        'text' => "Don't want to go to work(((o(*ﾟ▽ﾟ*)o)))",
+    ],
+    [
+        'text' => '66666666666',
+    ],
+    [
+        'text' => 'Lanshou Xiangguo(*////▽////*)q',
+    ],
+    [
+        'text' => 'Eat delicious food and drink spicy food♔♔♔',
+    ],
+    [
+        'text' => 'Be lenient to yourself and strict to others(o^^o)',
+    ],
+    [
+        'text' => 'Make more money and get angry lessO(∩_∩)O~~',
+    ],
+    [
+        'text' => 'There is no solution to life, drink a latte',
     ],
 ];
