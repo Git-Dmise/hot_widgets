496f3717d1f8dd403224959a46558bfe5d6cd60b
Dmise <2894467595@qq.com>
2024-12-11 10:20:42 +0800

    code update

--- app/Http/Controllers/Webhook/AppConfigController.php
+++ app/Http/Controllers/Webhook/AppConfigController.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Controllers\Webhook;
 
+use Illuminate\Http\Response;
+
 class AppConfigController extends Controller
 {
-    public function store()
+    public function store(): Response
     {
-        dd(1);
+        $data = $this->makeData();
+        $config = json_encode($data['config'], JSON_PRETTY_PRINT);
+
+        $filePath = app_path("Libs/config/config-{$data['version']}.json");
+
+        file_put_contents($filePath, $config);
+
+        return response()->noContent();
+    }
+
+    public function makeData(): array
+    {
+        return $this->request->validate([
+            'version' => ['required', 'string'],
+            'config' => ['required', 'array'],
+        ]);
     }
 }
