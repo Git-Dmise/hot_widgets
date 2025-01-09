c8e63bfac98ecded3c11b1034439472847451ff9
Dmise <2894467595@qq.com>
2025-01-09 17:54:13 +0800

    code update

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('init', [ServerController::class, 'index']); // 获取时间戳
-Route::get('version/force-update', [VersionConfigController::class, 'force_update']); // 强制更新检测
+Route::get('version/force-update', [VersionConfigController::class, 'force_update']);   // 强制更新检测
 
 Route::middleware(['api.sign.check'])->group(function () {
     Route::get('version/config', [VersionConfigController::class, 'config']); // 获取版本配置
@@ -34,7 +34,7 @@ Route::middleware(['api.sign.check'])->group(function () {
     });
 
     // 灵动岛
-    Route::get('animation/index', [AnimationController::class, 'index']); // 动画岛列表
+    Route::get('animation/index', [AnimationController::class, 'index']);   // 动画岛列表
     Route::get('barrage/index', [BarrageController::class, 'index']);   // 弹幕岛列表
     Route::get('start/index', [StartController::class, 'index']); // 启动岛列表
 });
